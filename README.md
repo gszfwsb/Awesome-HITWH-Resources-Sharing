7aa7601a494f41283e8d35d0d29270dd88e5c53d
WSB <46512956+hitergszf@users.noreply.github.com>
2020-12-20 18:16:22 +0800

    upload 并行计算，云计算和大数据

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Introduction :grin: :grin: :grin: 
+# Introduction
 
 
 This project aims to provide high quality resource sharing to HITwh students
-## contents： :scream: :scream: :scream: 
+## Content：
 1. Slides & Memos
 2. Lecture notes
 
@@ -13,18 +13,19 @@ This project aims to provide high quality resource sharing to HITwh students
 
 - content：
 
-  ![image-20201219102247912](https://i.loli.net/2020/12/19/xHFTGlybQaSmd5i.png)
+  ![image-20201220180620753](https://i.loli.net/2020/12/20/5nZpCX6OxjsqELI.png)
 
 ## 计软语你讲师团  person in charge: wsb
-- bilibili homepage：HITwh软18语你讲师团 :eyes: :eyes: :eyes:
+- bilibili homepage：HITwh软18语你讲师团
 - https://space.bilibili.com/478354813?from=search&seid=5980619953709063940
 
 members(names are not listed in order)
-1. wsb, zwj: 离散数学,
+1. wsb, zwj: 离散数学（近世代数）,
 2. wys, lkw: 数字逻辑设计,
 3. lyf: Java,
 4. wec, zycf: 数据结构,
 5. lkh: 计算机组成原理,
 6. hlx: 软件工程概论,
-7. xrs: Python Django全栈 tutorial
+7. xrs: Python Django tutorial
 
+> plz contact me if you want to upload your resources to support this project! 
---
+++ 软件学院——课件和笔记/大数据（大数据）/Lab9/Mega_Case_Study/mega_case_study.py
@@ -0,0 +1,93 @@
+# Mega Case Study - Make a Hybrid Deep Learning Model
+
+
+
+# Part 1 - Identify the Frauds with the Self-Organizing Map
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+# Training the SOM
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+# Visualizing the results
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+
+# Finding the frauds
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(5,3)], mappings[(8,3)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
+
+
+
+# Part 2 - Going from Unsupervised to Supervised Deep Learning
+
+# Creating the matrix of features
+customers = dataset.iloc[:, 1:].values
+
+# Creating the dependent variable
+is_fraud = np.zeros(len(dataset))
+for i in range(len(dataset)):
+    if dataset.iloc[i,0] in frauds:
+        is_fraud[i] = 1
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+customers = sc.fit_transform(customers)
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu', input_dim = 15))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
+
+# Predicting the probabilities of frauds
+y_pred = classifier.predict(customers)
+y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
+y_pred = y_pred[y_pred[:, 1].argsort()]
